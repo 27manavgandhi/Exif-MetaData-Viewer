1392303fe97509f726d3377c1575312523961753
27manavgandhi <27manavgandhi@gmail.com>
2024-01-22 20:02:13 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# GitHub EXIF Metadata Viewer
+
+## Overview
+
+GitHub EXIF Metadata Viewer is a straightforward tool for extracting and displaying EXIF metadata from images within GitHub repositories. This project aims to enhance the GitHub browsing experience by allowing users to conveniently inspect detailed information about the images they encounter.
+
+## Live Link
+
+[GitHub EXIF Metadata Viewer - Live Demo](#) <!-- Ad>
+
+## Demo Pictures
+
+
+
+## How to Run
+
+To run this code locally, follow these simple steps:
+
+1. Clone the repository to your local machine:
+
+    ```bash
+    git clone https://github.com/your-username/github-exif-metadata-viewer.git
+    ```
+
+2. Change into the project directory:
+
+    ```bash
+    cd github-exif-metadata-viewer
+    ```
+
+3. Install the required dependencies:
+
+    ```bash
+    npm install
+    ```
+
+4. Start the application:
+
+    ```bash
+    npm start
+    ```
+
+The application will now be accessible at [http://localhost:3000](http://localhost:3000). Open this link in your web browser to use the GitHub EXIF Metadata Viewer locally. Explore and enjoy the enhanced image browsing experience!
---
+++ src/Constants.js
@@ -0,0 +1,59 @@
+import FilterPixelOne from "./assets/FilterPixelOne.jpg";
+import FilterPixelTwo from "./assets/FilterPixelTwo.jpg";
+import FilterPixelThree from "./assets/FilterPixelThree.jpg";
+import FilterPixelFour from "./assets/FilterPixelFour.jpg";
+import FilterPixelFive from "./assets/FilterPixelFive.jpg";
+import FilterPixelSix from "./assets/FilterPixelSix.jpg";
+import FilterPixelSeven from "./assets/FilterPixelSeven.jpg";
+import FilterPixelEight from "./assets/FilterPixelEight.jpg";
+import FilterPixelNine from "./assets/FilterPixelNine.jpg";
+import FilterPixelTen from "./assets/FilterPixelTen.jpg";
+import FilterPixelEleven from "./assets/FilterPixelEleven.jpg";
+import FilterPixelTwelve from "./assets/FilterPixelTwelve.jpg";
+import FilterPixelThirteen from "./assets/FilterPixelThirteen.jpg";
+
+
+export const imageUri =
+  "https://media.licdn.com/dms/image/C4E0BAQGGwfK26wjkjw/company-logo_200_200/0/1632041566461/filterpixel_logo?e=2147483647&v=beta&t=hY5LHdGRJiNZ8NcZH11EU08QsnJVa8EmuhAHFpPP4Uc";
+
+export const arrayDummyImage = [
+  {
+    uri: FilterPixelOne,
+  },
+  {
+    uri: FilterPixelTwo,
+  },
+  {
+    uri: FilterPixelThree,
+  },
+  {
+    uri: FilterPixelFour,
+  },
+  {
+    uri: FilterPixelFive,
+  },
+  {
+    uri: FilterPixelSix,
+  },
+  {
+    uri: FilterPixelSeven,
+  },
+  {
+    uri: FilterPixelEight,
+  },
+  {
+    uri: FilterPixelNine,
+  },
+  {
+    uri: FilterPixelTen,
+  },
+  {
+    uri: FilterPixelEleven,
+  },
+  {
+    uri: FilterPixelTwelve,
+  },
+  {
+    uri: FilterPixelThirteen,
+  },
+];
